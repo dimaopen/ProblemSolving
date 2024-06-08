c239bf0d0cc16f400669b94991ea75235db2ce31
Dmitry Openkov <dimaopen@gmail.com>
2024-06-08 17:55:39 +0300

    #208 Trie using array with Option instead of Map

--- src/main/scala/contest/leetcode/tree/Trie208.scala
+++ src/main/scala/contest/leetcode/tree/Trie208.scala
@@ -1,34 +1,41 @@
 package contest.leetcode.tree
 
-import scala.collection.mutable
-
 class Trie208:
 
-  private class Node(val c: Char, var present: Boolean, val children: mutable.Map[Char, Node]) extends Ordered[Node]:
-    def compare(that: Node): Int = this.c - that.c
+  private class Node(
+    var isWord: Boolean = false,
+    val children: Array[Option[Node]] = Array.fill('z' - 'a' + 1)(None)
+  ):
+    def getChildOrCreateOne(char: Char): Node =
+      getChild(char) match
+        case None =>
+          val node = Node()
+          children(char - 'a') = Some(node)
+          node
+        case Some(node) => node
 
-  private val root: Node = Node(Char.MinValue, true, mutable.Map.empty)
+    def getChild(char: Char): Option[Node] = children(char - 'a')
+
+  private val root: Node = Node()
 
   def insert(word: String): Unit =
-    val node = word.foldLeft(root) { case (parent, char) =>
-      val node = parent.children.getOrElseUpdate(char, Node(char, present = false, mutable.Map.empty))
-      node
-    }
-    node.present = true
+    word.foldLeft(root) { case (parent, char) =>
+      parent.getChildOrCreateOne(char)
+    }.isWord = true
 
   def search(word: String): Boolean =
     val nodes = takeNodes(word)
-    nodes.length == word.length && nodes.last.present
+    nodes.length == word.length && nodes.last.isWord
 
   def startsWith(prefix: String): Boolean =
     val nodes = takeNodes(prefix)
     nodes.length == prefix.length
 
-  private def takeNodes(word: String) =
-    IndexedSeq.unfold((root.children, word)) { case (nodes, word) =>
-      val nodeOpt = word.headOption.fold(None)(nodes.get)
+  private def takeNodes(word: String): IndexedSeq[Node] =
+    IndexedSeq.unfold((root, word)) { case (node, word) =>
+      val nodeOpt: Option[Node] = word.headOption.fold(None)(node.getChild)
       nodeOpt.map { node =>
-        (node, (node.children, word.tail))
+        (node, (node, word.tail))
       }
     }
 
